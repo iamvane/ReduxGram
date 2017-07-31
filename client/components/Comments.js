6ebf454c91acb82f6a6fea05e886f9edf23546a0
IAmVane <vane.charles24@gmail.com>
2017-07-31 16:15:15 -0400

    updating comment state in store

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,7 +1,12 @@
 import React, {Component} from 'react';
 
 class Comments extends Component{
-	
+	constructor(props){
+		super(props);
+
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
 	renderComment(comment, i){
 		return(
 			<div className="comment" key={i}>
@@ -12,13 +17,21 @@ class Comments extends Component{
 				</p>
 			</div>
 		)
-	};
+	}
+
+	handleSubmit(e){
+		e.preventDefault();
+		const {postId} = this.props.params;
+		const author = this.refs.author.value;
+		const comment = this.refs.comment.value;
+		this.props.addComment(postId, author, comment);
+	}
 
 	render(){
 		return(
 			<div className="comments">
 				{this.props.postComments.map(this.renderComment)}
-				<form ref="commentForm" className="comment-form">
+				<form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
 					<input type="text" ref="author" placeholder="author"/>
 					<input type="text" ref="comment" placeholder="comment"/>
 					<input type="submit" hidden/>	
