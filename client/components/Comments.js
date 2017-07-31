79b60a93ec81230e511b7a532672e9adbaaa3ce2
IAmVane <vane.charles24@gmail.com>
2017-07-31 16:33:33 -0400

    implemented comments deletion

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -5,6 +5,7 @@ class Comments extends Component{
 		super(props);
 
 		this.handleSubmit = this.handleSubmit.bind(this);
+		this.renderComment = this.renderComment.bind(this);
 	}
 
 	renderComment(comment, i){
@@ -13,7 +14,7 @@ class Comments extends Component{
 				<p>
 					<strong>{comment.user}</strong>
 					{comment.text}
-					<button className="remove-comment">&times;</button>
+					<button className="remove-comment" onClick={this.props.removeComment.bind(null, this.props.params.postId, i)}>&times;</button>
 				</p>
 			</div>
 		)
@@ -25,6 +26,7 @@ class Comments extends Component{
 		const author = this.refs.author.value;
 		const comment = this.refs.comment.value;
 		this.props.addComment(postId, author, comment);
+		this.refs.commentForm.reset();
 	}
 
 	render(){
